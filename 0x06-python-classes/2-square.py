b7cf7dc2b7a1f77454a73d2e376f551582151943
“Eghele” <eghele14@gmail.com>
2022-12-25 13:42:10 -0800

    wow

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-
-"""Define a class Square."""
+"""Module that contains the ``Square`` class definition.
+"""
 
 
 class Square:
-    """Represent a square."""
-
-def __init__(self, size=0):
-    """Initialize a new Square.
-    Args:
-    size (int): The size of the new square.
+    """Class ``Square`` definition.
     """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
+    def __init__(self, size=0):
+        """Initializes the attributes of the ``Square`` class.
+        Args:
+            size (int): size of the ``Square``.
+        """
+        if type(size) == int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError('size must be >= 0')
+        else:
+            raise TypeError('size must be an integer')
